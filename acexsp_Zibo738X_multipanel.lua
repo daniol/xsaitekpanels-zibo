15b026690436c7853580a4e5258539c143d44969
daniol <dani88elx@gmail.com>
2020-06-19 10:08:28 +0200

    initial upload (v. 1.11)

---
+++ acexsp_Zibo738X_multipanel.lua
@@ -0,0 +1,169 @@
+--[[
+	Zibo Boeing 737-800
+	Version: 1.11
+--]]
+if PLANE_ICAO == "B738" then
+	-- ************************************************************************************************************************************************************************************************
+	-- GLOBAL VARIABLES
+	AceXSP_Version = "1.9"
+	
+	-- Global datarefs
+	dataref("XSP_FNBUTTON", "bgood/xsaitekpanels/fnbutton/status", "readonly")
+	
+	-- Light status datarefs
+	dataref("ZIBO_MCP_CMD_A_STATUS", "laminar/B738/autopilot/cmd_a_status", "readonly")
+	dataref("ZIBO_MCP_CMD_B_STATUS", "laminar/B738/autopilot/cmd_b_status", "readonly")
+	dataref("ZIBO_MCP_HDG_SEL_STATUS", "laminar/B738/autopilot/hdg_sel_status", "readonly")
+	dataref("ZIBO_MCP_LNAV_STATUS", "laminar/B738/autopilot/lnav_status", "readonly")
+	dataref("ZIBO_MCP_N1_STATUS", "laminar/B738/autopilot/n1_status", "readonly")
+	dataref("ZIBO_MCP_SPEED_STATUS", "laminar/B738/autopilot/speed_status1", "readonly")
+	dataref("ZIBO_MCP_LVL_CGH_STATUS", "laminar/B738/autopilot/lvl_chg_status", "readonly")
+	dataref("ZIBO_MCP_ALT_HLD_STATUS", "laminar/B738/autopilot/alt_hld_status", "readonly")
+	dataref("ZIBO_MCP_VNAV_STATUS", "laminar/B738/autopilot/vnav_status1", "readonly")
+	dataref("ZIBO_MCP_VS_STATUS", "laminar/B738/autopilot/vs_status", "readonly")
+	dataref("ZIBO_MCP_VORLOC_STATUS", "laminar/B738/autopilot/vorloc_status", "readonly")
+	dataref("ZIBO_MCP_APP_STATUS", "laminar/B738/autopilot/app_status", "readonly")
+	
+	-- Integers used to light up Multipanel buttons
+	dataref("XSP_Integer1", "bgood/xsaitekpanels/sharedata/integer1", "writable")	-- AP
+	dataref("XSP_Integer2", "bgood/xsaitekpanels/sharedata/integer2", "writable")	-- HDG
+	dataref("XSP_Integer3", "bgood/xsaitekpanels/sharedata/integer3", "writable")	-- NAV
+	dataref("XSP_Integer4", "bgood/xsaitekpanels/sharedata/integer4", "writable")	-- IAS
+	dataref("XSP_Integer5", "bgood/xsaitekpanels/sharedata/integer5", "writable")	-- ALT
+	dataref("XSP_Integer6", "bgood/xsaitekpanels/sharedata/integer6", "writable")	-- VS
+	dataref("XSP_Integer7", "bgood/xsaitekpanels/sharedata/integer7", "writable")	-- APR
+	dataref("XSP_Integer8", "bgood/xsaitekpanels/sharedata/integer8", "writable")	-- REV
+	
+	-- ************************************************************************************************************************************************************************************************
+	-- PROGRAM START
+
+	logMsg ( "AceXSP: ** Starting AceXSP_Zibo738X MULTIPANEL version "..AceXSP_Version.." **" )
+		
+	-- ************************************************************************************************************************************************************************************************
+	-- MULTIPANEL COMMANDS
+	
+	-- AP
+	-- LED: XSP_Integer1
+	function AceXSP_Multi_Button_AP()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/autopilot/cmd_a_press")
+		else
+			command_once("laminar/B738/autopilot/cmd_b_press")			
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Multi_Button_AP", "Multi_Button_AP", "AceXSP_Multi_Button_AP()", "", "")
+	
+	-- IAS
+	-- LED: XSP_Integer4
+	function AceXSP_Multi_Button_IAS()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/autopilot/speed_press")
+		else
+			command_once("laminar/B738/autopilot/n1_press")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Multi_Button_IAS", "Multi_Button_IAS", "AceXSP_Multi_Button_IAS()", "", "")
+
+	-- --------------------------------------------------------------------------------------------
+	
+	-- ALT
+	-- LED: XSP_Integer5
+	function AceXSP_Multi_Button_ALT()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/autopilot/lvl_chg_press")
+		else
+			command_once("laminar/B738/autopilot/alt_hld_press")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Multi_Button_ALT", "Multi_Button_ALT", "AceXSP_Multi_Button_ALT()", "", "")
+
+	-- --------------------------------------------------------------------------------------------
+	
+	-- APR
+	-- LED: XSP_Integer7
+	function AceXSP_Multi_Button_APR()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/autopilot/app_press")
+		else
+			command_once("laminar/B738/autopilot/vorloc_press")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Multi_Button_APR", "Multi_Button_APR", "AceXSP_Multi_Button_APR()", "", "")
+	
+	-- ************************************************************************************************************************************************************************************************
+	-- REFRESH LIGHTS
+	
+	function AceXSP_RefreshLights()
+		-- AP
+		if ZIBO_MCP_CMD_A_STATUS == 1 or ZIBO_MCP_CMD_B_STATUS == 1 then
+			XSP_Integer1 = 2
+		else
+			XSP_Integer1 = 0
+		end
+
+		-- HDG
+		if ZIBO_MCP_HDG_SEL_STATUS == 0 then
+			XSP_Integer2 = 0
+		else
+			XSP_Integer2 = 2
+		end
+		
+		-- NAV
+		if ZIBO_MCP_LNAV_STATUS == 1 then
+			XSP_Integer3 = 2
+		else
+			XSP_Integer3 = 0
+		end
+
+		-- IAS
+		if ZIBO_MCP_N1_STATUS == 1 then
+			XSP_Integer4 = 1					-- N1 mode - blink
+		elseif ZIBO_MCP_SPEED_STATUS == 1  then
+			XSP_Integer4 = 2					-- SPD mode - solid
+		else
+			XSP_Integer4 = 0
+		end
+		
+		-- ALT
+		if ZIBO_MCP_LVL_CGH_STATUS == 1 then
+			XSP_Integer5 = 1					-- FLCH mode - blink
+		elseif ZIBO_MCP_ALT_HLD_STATUS == 1 then
+			XSP_Integer5 = 2					-- ALTHLD mode - solid
+		else
+			XSP_Integer5 = 0
+		end
+		
+		-- VS
+		if ZIBO_MCP_VS_STATUS == 1 then
+			XSP_Integer6 = 2
+		else
+			XSP_Integer6 = 0
+		end
+		
+		-- APP
+		if ZIBO_MCP_VORLOC_STATUS == 1 and ZIBO_MCP_APP_STATUS == 0 then
+			XSP_Integer7 = 1					-- VORLOC mode - blink
+		elseif ZIBO_MCP_VORLOC_STATUS == 0 and ZIBO_MCP_APP_STATUS == 1 then
+			XSP_Integer7 = 2					-- APP mode - solid
+		else
+			XSP_Integer7 = 0
+		end
+		
+		-- REV
+		if ZIBO_MCP_VNAV_STATUS == 1 then
+			XSP_Integer8 = 2
+		else
+			XSP_Integer8 = 0
+		end
+		return
+	end
+
+	-- --------------------------------------------------------------------------------------------
+
+	do_every_frame("AceXSP_RefreshLights()")
+	
+end
---
+++ acexsp_Zibo738X_switchpanel.lua
@@ -0,0 +1,281 @@
+--[[
+	Zibo Boeing 737-800
+	Version: 1.11
+--]]
+if PLANE_ICAO == "B738" then
+    
+    local function sleep(n)
+        local t0 = os.clock()
+        while os.clock() - t0 <= n do end
+    end
+    
+	-- ************************************************************************************************************************************************************************************************
+	-- GLOBAL VARIABLES
+	AceXSP_Version = "1.9"
+	
+	-- Global datarefs
+	dataref("XSP_FNBUTTON", "bgood/xsaitekpanels/fnbutton/status", "readonly")
+	
+	-- Switch datarefs
+	dataref("ZIBO_AUTOBRAKE_POS", "laminar/B738/autobrake/autobrake_pos", "readonly")
+	dataref("ZIBO_IRS_LEFT", "laminar/B738/toggle_switch/irs_left", "readonly")
+	dataref("ZIBO_IRS_RIGHT", "laminar/B738/toggle_switch/irs_right", "readonly")
+	dataref("ZIBO_FUEL_LEFT1", "laminar/B738/fuel/fuel_tank_pos_lft1", "readonly")
+	dataref("ZIBO_FUEL_LEFT2", "laminar/B738/fuel/fuel_tank_pos_lft2", "readonly")
+	dataref("ZIBO_FUEL_RIGHT1", "laminar/B738/fuel/fuel_tank_pos_rgt1", "readonly")
+	dataref("ZIBO_FUEL_RIGHT2", "laminar/B738/fuel/fuel_tank_pos_rgt2", "readonly")
+	dataref("ZIBO_FUEL_CENTER1", "laminar/B738/fuel/fuel_tank_pos_ctr1", "readonly")
+	dataref("ZIBO_FUEL_CENTER2", "laminar/B738/fuel/fuel_tank_pos_ctr2", "readonly")
+		
+	-- ************************************************************************************************************************************************************************************************
+	-- PROGRAM START
+
+	logMsg ( "AceXSP: ** Starting AceXSP_Zibo738X SWITCHPANEL version "..AceXSP_Version.." **" )
+		
+	-- ************************************************************************************************************************************************************************************************
+	-- SWITCHPANEL COMMANDS
+	
+	-- Starter - Autobrake
+	function AceXSP_Switch_Starter_Off()					-- RTO
+		if (ZIBO_AUTOBRAKE_POS ~= 0) then
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Starter_Off", "Switch_Starter_Off", "AceXSP_Switch_Starter_Off()", "", "")
+	function AceXSP_Switch_Starter_R()						-- OFF
+		if (ZIBO_AUTOBRAKE_POS ~= 1) then
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Starter_R", "Switch_Starter_R", "AceXSP_Switch_Starter_R()", "", "")
+	function AceXSP_Switch_Starter_L()						-- 1
+		if (ZIBO_AUTOBRAKE_POS ~= 2) then
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Starter_L", "Switch_Starter_L", "AceXSP_Switch_Starter_L()", "", "")
+	function AceXSP_Switch_Starter_Both()					-- 2
+		if (ZIBO_AUTOBRAKE_POS ~= 3) then
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Starter_Both", "Switch_Starter_Both", "AceXSP_Switch_Starter_Both()", "", "")
+	function AceXSP_Switch_Starter_Start()					-- 3
+		if (ZIBO_AUTOBRAKE_POS ~= 4) then
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_dn")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+			command_once("laminar/B738/knob/autobrake_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Starter_Start", "Switch_Starter_Start", "AceXSP_Switch_Starter_Start()", "", "")
+
+	function AceXSP_Switch_Bat_On()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/switch/battery_dn")
+                        command_once("laminar/B738/button_switch_cover02")
+		else
+			command_once("laminar/B738/toggle_switch/gpu_dn")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Bat_On", "Switch_Bat_On", "AceXSP_Switch_Bat_On()", "", "")
+
+	function AceXSP_Switch_Bat_Off()
+		if XSP_FNBUTTON == 0 then
+                        command_once("laminar/B738/button_switch_cover02")
+                        sleep(0.1)
+			command_once("laminar/B738/switch/battery_up")
+		else
+			command_once("laminar/B738/toggle_switch/gpu_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Bat_Off", "Switch_Bat_Off", "AceXSP_Switch_Bat_Off()", "", "")
+
+	function AceXSP_Switch_Alt_On()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/toggle_switch/gen1_dn")
+			command_once("laminar/B738/toggle_switch/gen2_dn")
+		else
+			command_once("laminar/B738/toggle_switch/apu_gen1_dn")
+			command_once("laminar/B738/toggle_switch/apu_gen2_dn")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Alt_On", "Switch_Alt_On", "AceXSP_Switch_Alt_On()", "", "")
+
+	function AceXSP_Switch_Alt_Off()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/toggle_switch/gen1_up")
+			command_once("laminar/B738/toggle_switch/gen2_up")
+		else
+			command_once("laminar/B738/toggle_switch/apu_gen1_up")
+			command_once("laminar/B738/toggle_switch/apu_gen2_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Alt_Off", "Switch_Alt_Off", "AceXSP_Switch_Alt_Off()", "", "")
+
+	function AceXSP_Switch_Avionics_On()
+		if (ZIBO_IRS_LEFT ~= 2) or (ZIBO_IRS_RIGHT ~= 2) then
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+			command_once("laminar/B738/toggle_switch/irs_L_right")
+			command_once("laminar/B738/toggle_switch/irs_L_right")
+			command_once("laminar/B738/toggle_switch/irs_R_right")
+			command_once("laminar/B738/toggle_switch/irs_R_right")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Avionics_On", "Switch_Avionics_On", "AceXSP_Switch_Avionics_On()", "", "")
+	function AceXSP_Switch_Avionics_Off()
+		--[[
+		if (ZIBO_IRS_LEFT ~= 0) or (ZIBO_IRS_RIGHT ~= 0) then
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_L_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+			command_once("laminar/B738/toggle_switch/irs_R_left")
+		end
+		--]]
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Avionics_Off", "Switch_Avionics_Off", "AceXSP_Switch_Avionics_Off()", "", "")
+
+	function AceXSP_Switch_Fuel_On()
+		if XSP_FNBUTTON == 0 then
+			if ZIBO_FUEL_LEFT1 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_lft1") end
+			if ZIBO_FUEL_LEFT2 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_lft2") end
+			if ZIBO_FUEL_RIGHT1 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt1") end
+			if ZIBO_FUEL_RIGHT2 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt2") end
+		else
+			if ZIBO_FUEL_LEFT1 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_lft1") end
+			if ZIBO_FUEL_LEFT2 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_lft2") end
+			if ZIBO_FUEL_RIGHT1 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt1") end
+			if ZIBO_FUEL_RIGHT2 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt2") end
+			if ZIBO_FUEL_CENTER1 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_ctr1") end
+			if ZIBO_FUEL_CENTER2 ~= 1 then command_once("laminar/B738/toggle_switch/fuel_pump_ctr2") end
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Fuel_On", "Switch_Fuel_On", "AceXSP_Switch_Fuel_On()", "", "")
+
+	function AceXSP_Switch_Fuel_Off()
+		if XSP_FNBUTTON == 0 then
+			if ZIBO_FUEL_LEFT1 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_lft1") end
+			if ZIBO_FUEL_LEFT2 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_lft2") end
+			if ZIBO_FUEL_RIGHT1 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt1") end
+			if ZIBO_FUEL_RIGHT2 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt2") end
+		else
+			if ZIBO_FUEL_LEFT1 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_lft1") end
+			if ZIBO_FUEL_LEFT2 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_lft2") end
+			if ZIBO_FUEL_RIGHT1 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt1") end
+			if ZIBO_FUEL_RIGHT2 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_rgt2") end
+			if ZIBO_FUEL_CENTER1 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_ctr1") end
+			if ZIBO_FUEL_CENTER2 ~= 0 then command_once("laminar/B738/toggle_switch/fuel_pump_ctr2") end
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Fuel_Off", "Switch_Fuel_Off", "AceXSP_Switch_Fuel_Off()", "", "")
+
+	function AceXSP_Switch_Panel_On()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/toggle_switch/cockpit_dome_dn")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_dn")
+		else
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Panel_On", "Switch_Panel_On", "AceXSP_Switch_Panel_On()", "", "")
+
+	function AceXSP_Switch_Panel_Off()
+		if XSP_FNBUTTON == 0 then
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_dn")
+		else
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_up")
+			command_once("laminar/B738/toggle_switch/cockpit_dome_dn")
+		end
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Panel_Off", "Switch_Panel_Off", "AceXSP_Switch_Panel_Off()", "", "")
+
+	
+	-- LAND LIGHTS
+	function AceXSP_Switch_Land_On()
+		command_once("laminar/B738/switch/land_lights_ret_left_dn")
+		command_once("laminar/B738/switch/land_lights_ret_left_dn")
+		command_once("laminar/B738/switch/land_lights_ret_right_dn")
+		command_once("laminar/B738/switch/land_lights_ret_right_dn")
+		command_once("laminar/B738/switch/rwy_light_left_on")
+		command_once("laminar/B738/switch/rwy_light_right_on")
+		set("laminar/B738/switch/land_lights_left_pos", 1)
+		set("laminar/B738/switch/land_lights_right_pos", 1)
+		set("laminar/B738/switch/land_lights_ret_left_pos", 2)
+		set("laminar/B738/switch/land_lights_ret_right_pos", 2)
+		set_array("sim/cockpit2/switches/landing_lights_switch", 0, 1)
+		set_array("sim/cockpit2/switches/landing_lights_switch", 3, 1)
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Land_On", "Switch_Land_On", "AceXSP_Switch_Land_On()", "", "")
+	
+	function AceXSP_Switch_Land_Off()
+		command_once("laminar/B738/switch/land_lights_ret_left_up")
+		command_once("laminar/B738/switch/land_lights_ret_left_up")
+		command_once("laminar/B738/switch/land_lights_ret_right_up")
+		command_once("laminar/B738/switch/land_lights_ret_right_up")	
+		command_once("laminar/B738/switch/rwy_light_left_off")
+		command_once("laminar/B738/switch/rwy_light_right_off")
+		set("laminar/B738/switch/land_lights_left_pos", 0) 
+		set("laminar/B738/switch/land_lights_right_pos", 0)
+		set("laminar/B738/switch/land_lights_ret_left_pos", 0)
+		set("laminar/B738/switch/land_lights_ret_right_pos", 0)
+		set_array("sim/cockpit2/switches/landing_lights_switch", 0, 0)
+		set_array("sim/cockpit2/switches/landing_lights_switch", 3, 0)
+		return
+	end
+	create_command("FlyWithLua/AceXSP/Switch_Land_Off", "Switch_Land_Off", "AceXSP_Switch_Land_Off()", "", "")
+		
+end
